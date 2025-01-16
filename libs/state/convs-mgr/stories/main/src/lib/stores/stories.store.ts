d6eabc15418b7f81336dbec230cb6f2c71cacd82
Brian Baliach <brianbaliach@gmail.com>
2025-01-16 14:12:06 +0300

    fixed loading organisations issue

--- libs/state/convs-mgr/stories/main/src/lib/stores/stories.store.ts
+++ libs/state/convs-mgr/stories/main/src/lib/stores/stories.store.ts
@@ -1,10 +1,10 @@
 import { Injectable } from '@angular/core';
 
-import { Repository, DataService } from '@ngfi/angular';
-import { DataStore }  from '@ngfi/state';
+import { DataService, Repository } from '@ngfi/angular';
+import { DataStore } from '@ngfi/state';
 
-import { of } from 'rxjs'
-import { tap, throttleTime, switchMap } from 'rxjs/operators';
+import { of } from 'rxjs';
+import { switchMap, tap, throttleTime } from 'rxjs/operators';
 
 import { Logger } from '@iote/bricks-angular';
 
@@ -14,38 +14,38 @@ import { Story } from '@app/model/convs-mgr/stories/main';
 import { ActiveOrgStore } from '@app/private/state/organisation/main';
 
 @Injectable()
-export class StoriesStore extends DataStore<Story>
-{
+export class StoriesStore extends DataStore<Story> {
   protected store = 'stories-store';
   protected _activeRepo: Repository<Story>;
 
   private _activeOrg: Organisation;
-  
+
   // Question to dev's reviewing:
   //   Will this always get all the organisations?
-  //     i.e. Even if no organisations need to be loaded for a specific piece of functionaly e.g. invites, do we still load all organisations?
-  //
-  // Answer: No, as Angular's DI engine is lazy, meaning it will only initialise services the first time they are called.
-  constructor(_org$$: ActiveOrgStore,
-              _repoFac: DataService,
-              _logger: Logger)
-  {
-    super("always", _logger);
+  //     i.e. Even if no organisations need to be loaded for a specific piece of functionaly e.g. invites, do we still
+  // load all organisations?  Answer: No, as Angular's DI engine is lazy, meaning it will only initialise services the
+  // first time they are called.
+  constructor(_org$$: ActiveOrgStore, _repoFac: DataService, _logger: Logger) {
+    super('always', _logger);
 
-    const data$ = _org$$.get()
-                    .pipe(
-                      tap((org: Organisation) => this._activeOrg  = org),
-                      tap((org: Organisation) => this._activeRepo = _repoFac.getRepo<Story>(`orgs/${org.id}/stories`)),
-                      switchMap((org: Organisation) => 
-                        org ? this._activeRepo.getDocuments() : of([] as Story[])),
-                      throttleTime(500, undefined, { leading: true, trailing: true }));
+    const data$ = _org$$.get().pipe(
+      tap((org: Organisation) => (this._activeOrg = org)),
+      tap(
+        (org: Organisation) =>
+          (this._activeRepo = _repoFac.getRepo<Story>(`orgs/${org.id}/stories`))
+      ),
+      switchMap((org: Organisation) =>
+        org ? this._activeRepo.getDocuments() : of([] as Story[])
+      ),
+      throttleTime(500, undefined, { leading: true, trailing: true })
+    );
 
-    this._sbS.sink = data$.subscribe(properties => {
+    this._sbS.sink = data$.subscribe((properties) => {
       this.set(properties, 'UPDATE - FROM DB');
     });
   }
 
-  publish(story: Story){
+  publish(story: Story) {
     story.publishedOn = new Date();
     return this._activeRepo.write(story, story.id!);
   }
