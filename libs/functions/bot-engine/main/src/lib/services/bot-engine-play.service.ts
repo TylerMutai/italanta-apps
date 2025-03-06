d868682d32ee4f8203f5ab66f81195b871271e1e
Brian Baliach <brianbaliach@gmail.com>
2025-03-06 12:09:05 +0300

    added default bot message when anyone interacts with the bot

--- libs/functions/bot-engine/main/src/lib/services/bot-engine-play.service.ts
+++ libs/functions/bot-engine/main/src/lib/services/bot-engine-play.service.ts
@@ -1,44 +1,51 @@
-import { HandlerTools } from "@iote/cqrs";
+import { HandlerTools } from '@iote/cqrs';
 
-import { Cursor } from "@app/model/convs-mgr/conversations/admin/system";
-import { FileMessage, Message, MessageDirection, TextMessage } from "@app/model/convs-mgr/conversations/messages";
-import { isMediaBlock, isOutputBlock, StoryBlock } from "@app/model/convs-mgr/stories/blocks/main";
-import { EndUser } from "@app/model/convs-mgr/conversations/chats";
-import { isFileMessage, MessageTypes } from "@app/model/convs-mgr/functions";
+import { Cursor } from '@app/model/convs-mgr/conversations/admin/system';
+import {
+  FileMessage,
+  Message,
+  MessageDirection,
+  TextMessage,
+} from '@app/model/convs-mgr/conversations/messages';
+import {
+  StoryBlock,
+  StoryBlockTypes,
+} from '@app/model/convs-mgr/stories/blocks/main';
+import { EndUser } from '@app/model/convs-mgr/conversations/chats';
+import { isFileMessage, MessageTypes } from '@app/model/convs-mgr/functions';
 
-import { CursorDataService } from "./data-services/cursor.service";
-import { MessagesDataService } from "./data-services/messages.service";
-import { VariablesDataService } from "./data-services/variables.service";
+import { CursorDataService } from './data-services/cursor.service';
+import { MessagesDataService } from './data-services/messages.service';
 
-import { MailMergeVariables } from "./variable-injection/mail-merge-variables.service";
-import { ProcessMessageService } from "./process-message/process-message.service";
-import { BlockToStandardMessage } from "../io/block-to-message-parser.class";
-import { ChatCommandsManager } from "./chat-commands/chat-commands-manager.service";
-import { BotMediaProcessService } from "./media/process-media-service";
+import { MailMergeVariables } from './variable-injection/mail-merge-variables.service';
+import { ProcessMessageService } from './process-message/process-message.service';
+import { BlockToStandardMessage } from '../io/block-to-message-parser.class';
+import { ChatCommandsManager } from './chat-commands/chat-commands-manager.service';
+import { BotMediaProcessService } from './media/process-media-service';
 
-import { IBotEnginePlay } from "./bot-engine.interface";
-import { ActiveChannel } from "../model/active-channel.service";
+import { IBotEnginePlay } from './bot-engine.interface';
+import { ActiveChannel } from '../model/active-channel.service';
+import { PhoneNumberUtil } from 'google-libphonenumber';
 
-import { __isCommand } from "../utils/isCommand";
+import { __isCommand } from '../utils/isCommand';
+import {
+  defaultMessage,
+  SupportedLanguages,
+} from '../utils/messages/defaultMessage';
 
 /**
  * When out chatbot receives a message from the end user, we need to figure out how to
  *  respond back to them.
- * 
+ *
  * This model is resposible for 'playing' the end user through the stories. @see {Story}.
  *  It receives the message and responds with the next block in the story.
  */
-export class BotEnginePlay implements IBotEnginePlay
-{
-  protected orgId: string;
-
-  private defaultStory: string;
-
+export class BotEnginePlay implements IBotEnginePlay {
   blockSent: StoryBlock;
-
-  private chatCommandsManager: ChatCommandsManager;
-
   sideOperations: Promise<any>[] = [];
+  protected orgId: string;
+  private defaultStory: string;
+  private chatCommandsManager: ChatCommandsManager;
 
   constructor(
     private _processMessageService$: ProcessMessageService,
@@ -46,141 +53,241 @@ export class BotEnginePlay implements IBotEnginePlay
     protected _msgService$: MessagesDataService,
     private _processMediaService$: BotMediaProcessService,
     protected _activeChannel: ActiveChannel,
-    protected _tools: HandlerTools) 
-  {
+    protected _tools: HandlerTools
+  ) {
     this.defaultStory = _activeChannel.channel.defaultStory;
 
     this.orgId = _activeChannel.channel.orgId;
 
-    this.chatCommandsManager = new ChatCommandsManager(_activeChannel, _processMessageService$, _tools);
-
+    this.chatCommandsManager = new ChatCommandsManager(
+      _activeChannel,
+      _processMessageService$,
+      _tools
+    );
   }
 
   /**
    * When out chatbot receives a message from the end user, we need to figure out how to
    *  respond back to them.
-   * 
+   *
    * This method is resposible for 'playing' the end user through the stories. @see {Story}.
    *  It receives the message and responds with the next block in the story.
    */
-  async play(message: Message, endUser: EndUser, currentCursor: Cursor | boolean)
-  {
+  async play(
+    message: Message,
+    endUser: EndUser,
+    currentCursor: Cursor | boolean
+  ) {
     // Save the message (batches the message to be saved to firebase later)
-    this._saveEndUserMessage(message, endUser.id);
+    // this._saveEndUserMessage(message, endUser.id);
 
     // Get the next block in the story
-    const { nextBlock, newCursor } = await this.__getNextBlock(endUser, currentCursor, message);
+    // const { nextBlock, newCursor } = await this.__getNextBlock(endUser, currentCursor, message);
 
     // Send the block back to the user
     //
-    // The chatbot has some asynchronous operations (which we dont have to wait for, in order to process the message) 
-    //      e.g. saving the messages to firebase 
-    // 
-    // So, to ensure faster response to end users, we store all these operations to an array and resolve 
+    // The chatbot has some asynchronous operations (which we dont have to wait for, in order to process the message)
+    //      e.g. saving the messages to firebase
+    //
+    // So, to ensure faster response to end users, we store all these operations to an array and resolve
     //      them after we have responded to the user
 
-    // If the last block sent was media, we wait for 500ms 
+    // If the last block sent was media, we wait for 500ms
     //  before replying as media processing can take a while
     // if(this.blockSent && isMediaBlock(this.blockSent.type) && !isMediaBlock(nextBlock.type)) {
     //   await new Promise(resolve => setTimeout(resolve, 1500));
-    // } 
+    // }
 
-    await this.__reply(nextBlock, endUser, message);
+    const countryCodeLanguageCodeMap: { [key: number]: SupportedLanguages } = {
+      91: 'hi',
+      254: 'en',
+      1: 'en',
+      256: 'lg',
+      266: 'fr',
+      255: 'sw',
+      97: 'hi',
+      977: 'ne',
+      971: 'hi',
+      57: 'en',
+      55: 'en',
+      52: 'en',
+      39: 'es',
+      31: 'en',
+      593: 'en',
+      507: 'en',
+    };
 
-    this.__move(newCursor, endUser.id);
-
-    // Here is where the message chaining happens. 
-    //  If it is not an input block we replay until we get hit an input block. 
-    if (isOutputBlock(nextBlock.type)) {
-      this.blockSent = nextBlock;
-      return await this.play(null, endUser, newCursor);
+    try {
+      let phone = endUser.phoneNumber;
+      if (!phone.includes('+')) {
+        phone = `+${phone}`;
+      }
+      const phoneNumber = PhoneNumberUtil.getInstance().parse(phone);
+      await this.__reply(
+        {
+          blockIcon: '',
+          blockTitle: '',
+          deleted: false,
+          position: undefined,
+          type: StoryBlockTypes.TextMessage,
+          message:
+            defaultMessage[
+              countryCodeLanguageCodeMap[phoneNumber.getCountryCode()] || 'en'
+            ],
+        },
+        endUser
+      );
+    } catch (e) {
+      this._tools.Logger.log(() => `Error occurred: ${JSON.stringify(e)}`);
     }
 
-    const processMessageOps = this._processMessageService$.getSideOperations();
+    // this.__move(newCursor, endUser.id);
+    //
+    // // Here is where the message chaining happens.
+    // //  If it is not an input block we replay until we get hit an input block.
+    // if (isOutputBlock(nextBlock.type)) {
+    //   this.blockSent = nextBlock;
+    //   return await this.play(null, endUser, newCursor);
+    // }
 
-    this.addSideOperations(processMessageOps);
-    
-    // Resolve all pending operations.
-    await Promise.all(this.sideOperations);
+    // const processMessageOps = this._processMessageService$.getSideOperations();
+    //
+    // this.addSideOperations(processMessageOps);
+    //
+    // // Resolve all pending operations.
+    // await Promise.all(this.sideOperations);
   }
 
-  /**
-   * Responsible for returning the next block in the story.
-   */
-  private async __getNextBlock(endUser: EndUser, currentPosition: Cursor | boolean, message?: Message)
-  {
+  async __reply(nextBlock: StoryBlock, endUser: EndUser) {
+    // const varDataService = new VariablesDataService(this._tools, this.orgId, endUser.id);
 
-    this._tools.Logger.log(() => `[BotEnginePlay].__getNextBlock: Getting the next block... ${JSON.stringify(currentPosition)}`);
+    // const allVariables =  varDataService.getAllVariables(endUser);
 
-    if (message && message.type === MessageTypes.TEXT) {
-      const textMessage = message as TextMessage;
-
-      if (__isCommand(textMessage.text)) return this.chatCommandsManager.parseCommand(textMessage, endUser);
-
-    }
-
-    if (!currentPosition) {
-
-      this._tools.Logger.log(() => `[BotEnginePlay].__getNextBlock: New conversation, getting the first block instead.`);
-      // If the end user position does not exist then the conversation is new and we return the first block
-      return this._processMessageService$.getFirstBlock(this._tools, this.orgId, this.defaultStory);
-    } else {
-      this._tools.Logger.log(() => `[BotEnginePlay].__getNextBlock: Continuing conversation, getting the next block.`);
-
-      const endUserPosition = currentPosition as Cursor;
-      const currentStory  = endUserPosition.position.storyId;
-
-      // If the end user exists, then we continue the story by returning the next block.
-      return this._processMessageService$.resolveNextBlock(message, endUserPosition, endUser, this.orgId, currentStory, this._tools);
-    }
-  }
-
-  async __reply(nextBlock: StoryBlock, endUser: EndUser, message?: Message)
-  {
-    const varDataService = new VariablesDataService(this._tools, this.orgId, endUser.id);
-
-		const allVariables =  varDataService.getAllVariables(endUser);
-    
     // Inject Variables to the block
-    const mailMergedBlock = await this.__mailMergeVariables(nextBlock, allVariables);
+    // const mailMergedBlock = await this.__mailMergeVariables(nextBlock, allVariables);
 
-    this._tools.Logger.log(() => `Block to be sent: ${JSON.stringify(mailMergedBlock)}`)
+    this._tools.Logger.log(
+      () => `Block to be sent: ${JSON.stringify(nextBlock)}`
+    );
 
     // Save block to messages collection
-    this._saveBlockAsMessage(mailMergedBlock, endUser.id);
+    // this._saveBlockAsMessage(mailMergedBlock, endUser.id);
 
     // Reply To the end user
-    await this._sendBlockMessage(mailMergedBlock, endUser);
-  }
-
-  private async __mailMergeVariables(storyBlock: StoryBlock, variables: {[key:string]:any}) 
-  {
-    const newBlock = storyBlock;
-    // Initialize the variable injector service
-    const mailMergeVariables = new MailMergeVariables(this._tools);
-
-    // Find and replace any variables included in the block message
-    if(newBlock.message) newBlock.message = await mailMergeVariables.merge(storyBlock.message, this.orgId, variables);
-
-    return newBlock;
+    await this._sendBlockMessage(nextBlock, endUser);
   }
 
   /**
    * Updates the position of the end user with the block we send back to them
    */
-  __move(newPosition: Cursor, endUserId: string)
-  {
-    const moveCursor = this._cursorDataService$.updateCursor(endUserId, this._activeChannel.channel.orgId, newPosition);
+  __move(newPosition: Cursor, endUserId: string) {
+    const moveCursor = this._cursorDataService$.updateCursor(
+      endUserId,
+      this._activeChannel.channel.orgId,
+      newPosition
+    );
 
     this.sideOperations.push(moveCursor);
-  };
+  }
+
+  async __setFileMessageUrl(msg: FileMessage, endUserId: string) {
+    msg.url =
+      (await this._processMediaService$.getFileURL(
+        msg,
+        endUserId,
+        this._activeChannel
+      )) || null;
+
+    return msg;
+  }
+
+  async addSideOperations(operations: Promise<any>[]) {
+    this.sideOperations.push(...operations);
+  }
+
+  /**
+   * Responsible for returning the next block in the story.
+   */
+  private async __getNextBlock(
+    endUser: EndUser,
+    currentPosition: Cursor | boolean,
+    message?: Message
+  ) {
+    this._tools.Logger.log(
+      () =>
+        `[BotEnginePlay].__getNextBlock: Getting the next block... ${JSON.stringify(
+          currentPosition
+        )}`
+    );
+
+    if (message && message.type === MessageTypes.TEXT) {
+      const textMessage = message as TextMessage;
+
+      if (__isCommand(textMessage.text)) {
+        return this.chatCommandsManager.parseCommand(textMessage, endUser);
+      }
+    }
+
+    if (!currentPosition) {
+      this._tools.Logger.log(
+        () =>
+          `[BotEnginePlay].__getNextBlock: New conversation, getting the first block instead.`
+      );
+      // If the end user position does not exist then the conversation is new and we return the first block
+      return this._processMessageService$.getFirstBlock(
+        this._tools,
+        this.orgId,
+        this.defaultStory
+      );
+    } else {
+      this._tools.Logger.log(
+        () =>
+          `[BotEnginePlay].__getNextBlock: Continuing conversation, getting the next block.`
+      );
+
+      const endUserPosition = currentPosition as Cursor;
+      const currentStory = endUserPosition.position.storyId;
+
+      // If the end user exists, then we continue the story by returning the next block.
+      return this._processMessageService$.resolveNextBlock(
+        message,
+        endUserPosition,
+        endUser,
+        this.orgId,
+        currentStory,
+        this._tools
+      );
+    }
+  }
+
+  private async __mailMergeVariables(
+    storyBlock: StoryBlock,
+    variables: { [key: string]: any }
+  ) {
+    const newBlock = storyBlock;
+    // Initialize the variable injector service
+    const mailMergeVariables = new MailMergeVariables(this._tools);
+
+    // Find and replace any variables included in the block message
+    if (newBlock.message) {
+      newBlock.message = await mailMergeVariables.merge(
+        storyBlock.message,
+        this.orgId,
+        variables
+      );
+    }
+
+    return newBlock;
+  }
 
   /**
    * Parses @see StoryBlock and sends it as a message to the end user.
    */
-  private async _sendBlockMessage(newStoryBlock: StoryBlock, endUser: EndUser)
-  {
-    const outgoingMessage = this._activeChannel.parseOutMessage(newStoryBlock, endUser);
+  private async _sendBlockMessage(newStoryBlock: StoryBlock, endUser: EndUser) {
+    const outgoingMessage = this._activeChannel.parseOutMessage(
+      newStoryBlock,
+      endUser
+    );
 
     return this._activeChannel.send(outgoingMessage);
   }
@@ -188,9 +295,8 @@ export class BotEnginePlay implements IBotEnginePlay
   /**
    * Converts the block to our standardized message @see {Message} and saves it to the database
    */
-  private async _saveBlockAsMessage(storyBlock: StoryBlock, endUserId: string)
-  {
-    const endUserPhoneNumber = endUserId.split('_')[2]
+  private async _saveBlockAsMessage(storyBlock: StoryBlock, endUserId: string) {
+    const endUserPhoneNumber = endUserId.split('_')[2];
     const botMessage = this.__convertBlockToStandardMessage(storyBlock);
 
     botMessage.n = this._activeChannel.channel.n;
@@ -201,43 +307,34 @@ export class BotEnginePlay implements IBotEnginePlay
     this.sideOperations.push(saveMessage);
   }
 
-  private async __save(message: Message, endUserId: string) 
-  {
-      return this._msgService$.saveMessage(message, this.orgId, endUserId);
-  };
+  private async __save(message: Message, endUserId: string) {
+    return this._msgService$.saveMessage(message, this.orgId, endUserId);
+  }
 
-  private async _saveEndUserMessage (message: Message, endUserId: string) {
-    if(!message) return;
+  private async _saveEndUserMessage(message: Message, endUserId: string) {
+    if (!message) {
+      return;
+    }
 
-    if(isFileMessage(message.type) && !message.url) {
-      message = await this.__setFileMessageUrl(message as FileMessage, endUserId);
+    if (isFileMessage(message.type) && !message.url) {
+      message = await this.__setFileMessageUrl(
+        message as FileMessage,
+        endUserId
+      );
     }
     const saveEndUserMessage = this.__save(message, endUserId);
 
     this.sideOperations.push(saveEndUserMessage);
   }
 
-  async __setFileMessageUrl(msg: FileMessage, endUserId: string) { 
-
-    msg.url = await this._processMediaService$.getFileURL(msg, endUserId, this._activeChannel) || null;
-
-    return msg;
-  }
-
   /**
    * Converts the block to our standardized message @see {Message} so that we can have a common message
    *  structure that can easily be ready by third party applications
    */
-  private __convertBlockToStandardMessage(nextBlock: StoryBlock)
-  {
+  private __convertBlockToStandardMessage(nextBlock: StoryBlock) {
     const botMessage = new BlockToStandardMessage().convert(nextBlock);
     botMessage.direction = MessageDirection.FROM_CHATBOT_TO_END_USER;
 
     return botMessage;
-  }
-
-  async addSideOperations(operations: Promise<any>[])
-  {
-    this.sideOperations.push(...operations);
   }
 }
---
+++ libs/functions/bot-engine/main/src/lib/utils/messages/defaultMessage.ts
@@ -0,0 +1,76 @@
+export type SupportedLanguages =
+  | 'en'
+  | 'es'
+  | 'hi'
+  | 'fr'
+  | 'sw'
+  | 'vi'
+  | 'lg'
+  | 'ne';
+
+export const defaultMessage: { [key in SupportedLanguages]: string } = {
+  es:
+    '🚨 *Actualización importante de FarmBetter!* 🚨\n' +
+    '\n' +
+    '¡Hola! Tenemos un nuevo número de WhatsApp para los servicios de FarmBetter. Para seguir recibiendo actualizaciones y apoyo, envía *"Hola"* a nuestro nuevo número: *+254713290602*.\n' +
+    '\n' +
+    'Esto garantizará que sigas conectado con todos nuestros útiles consejos y servicios agrícolas.\n' +
+    '\n' +
+    '¡Gracias por ser parte de FarmBetter! 🌿🚜',
+  fr:
+    '🚨 *Mise à jour importante de FarmBetter !* 🚨\n' +
+    '\n' +
+    'Bonjour ! Nous avons un nouveau numéro WhatsApp pour les services FarmBetter. Pour continuer à recevoir des mises à jour et du support, envoyez *"Salut"* à notre nouveau numéro : *+254713290602*.\n' +
+    '\n' +
+    'Cela vous permettra de rester connecté à nos conseils et services agricoles utiles.\n' +
+    '\n' +
+    'Merci de faire partie de FarmBetter ! 🌿🚜',
+  hi:
+    '🚨 *फार्मबेटर से महत्वपूर्ण अपडेट!* 🚨\n' +
+    '\n' +
+    'नमस्ते! फार्मबेटर सेवाओं के लिए हमारे पास एक नया व्हाट्सएप नंबर है। अपडेट और समर्थन प्राप्त करना जारी रखने के लिए, कृपया हमारे नए नंबर *+254713290602* पर *"हाय"* भेजें।\n' +
+    '\n' +
+    'यह सुनिश्चित करेगा कि आप हमारे सभी उपयोगी खेती के सुझावों और सेवाओं से जुड़े रहें।\n' +
+    '\n' +
+    'फार्मबेटर का हिस्सा बनने के लिए धन्यवाद! 🌿🚜',
+  lg:
+    '🚨 *Kyukakasa ekikulu okuva mu FarmBetter!* 🚨\n' +
+    '\n' +
+    'Oli otya! Tulina ennamba ya WhatsApp empya ku buweereza bwa FarmBetter. Okusigala ofuna ebipya n\'obuyambi, tuma *"Oli otya"* ku nnamba yaffe empya: * +254713290602 *.\n' +
+    '\n' +
+    "Kino kijja kukakasa nti osigala okweyeza n'obuyambi bwaffe obw'amagera ku by'ennimiro.\n" +
+    '\n' +
+    'Webale okubeera parte ya FarmBetter! 🌿🚜',
+  ne:
+    '🚨 *FarmBetter बाट महत्त्वपूर्ण अपडेट !* 🚨\n' +
+    '\n' +
+    'नमस्ते! FarmBetter सेवाहरूको लागि हाम्रो नयाँ व्हाट्सएप नम्बर छ। अपडेटहरू र समर्थन प्राप्त गर्न जारी राख्नको लागि, कृपया हाम्रो नयाँ नम्बरमा * "हाइ" * पठाउनुहोस्: * + २५४७१३२९०६०२ *।\n' +
+    '\n' +
+    'यसले तपाईंलाई हाम्रो सबै उपयोगी खेती टिप्स र सेवाहरूसँग जडित राख्न सुनिश्चित गर्नेछ।\n' +
+    '\n' +
+    'FarmBetter को सदस्य भएकोमा धन्यवाद! 🌿🚜',
+  sw:
+    '🚨 *Sasisho Muhimu kutoka FarmBetter!* 🚨\n' +
+    '\n' +
+    'Habari! Tuna nambari mpya ya WhatsApp kwa huduma za FarmBetter. Ili kuendelea kupokea masasisho na msaada, tafadhali tuma *"Habari"* kwa nambari yetu mpya: *+254713290602*.\n' +
+    '\n' +
+    'Hii itahakikisha unakaa kuunganishwa na vidokezo vyetu vyote vya kusaidia kilimo na huduma.\n' +
+    '\n' +
+    'Asante kwa kuwa sehemu ya FarmBetter! 🌿🚜',
+  vi:
+    '🚨 *Thông báo quan trọng từ FarmBetter!* 🚨\n' +
+    '\n' +
+    'Xin chào! Chúng tôi có một số WhatsApp mới dành cho các dịch vụ FarmBetter. Để tiếp tục nhận thông tin cập nhật và hỗ trợ, vui lòng gửi *"Chào"* vào số mới của chúng tôi: *+254713290602*.\n' +
+    '\n' +
+    'Điều này sẽ đảm bảo bạn giữ kết nối với tất cả các mẹo và dịch vụ hữu ích của chúng tôi.\n' +
+    '\n' +
+    'Cảm ơn bạn đã là một phần của FarmBetter! 🌿🚜',
+  en:
+    '🚨 *Important Update from FarmBetter!* 🚨\n' +
+    '\n' +
+    'Hello! We have a new WhatsApp number for FarmBetter services. To continue receiving updates and support, please send *"Hi"* to our new number: *+254713290602*.\n' +
+    '\n' +
+    'This will ensure you stay connected with all our helpful farming tips and services.\n' +
+    '\n' +
+    'Thank you for being a part of FarmBetter! 🌿🚜',
+};
